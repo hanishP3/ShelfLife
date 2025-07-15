f87ef068c1c150033835fafc30574510814dea2d
hanish <phanish3000@gmail.com>
2025-07-15 22:47:38 +0530

    Add a basic navbar(Heading)

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import { Button } from "@/components/ui/button";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center"
-    onClick={()=>alert("The Button is clicked")}
-    >
-      <Button className="bg-black text-blue-200"
-      >Click me</Button>
+    <>
+    <div className="w-full bg-black font-bold">
+      <h1 className="text-5xl text-blue-50 ml-2.5">Shelf Life</h1>
     </div>
+      <div
+        className="flex min-h-svh flex-col items-center justify-center"
+        onClick={() => alert("The Button is clicked")}
+      >
+        <Button className="bg-black text-blue-200">Click me</Button>
+      </div>
+    </>
   );
 }
 
