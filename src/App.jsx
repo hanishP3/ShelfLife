60eb53b49c2ea344793047cf3e7f62931892f794
hanish <phanish3000@gmail.com>
2025-07-16 23:24:00 +0530

    add basic header

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,30 @@ import { Button } from "@/components/ui/button";
 function App() {
   return (
     <>
-    <div className="w-full bg-black font-bold">
-      <h1 className="text-5xl text-blue-50 ml-2.5">Shelf Life</h1>
-    </div>
-      <div
-        className="flex min-h-svh flex-col items-center justify-center"
-        onClick={() => alert("The Button is clicked")}
-      >
-        <Button className="bg-black text-blue-200">Click me</Button>
+      <div className="w-full bg-black font-bold">
+        <h1 className="text-5xl text-blue-50 ml-2.5">Shelf Life</h1>
+      </div>
+      <div className="h-lvh">
+        <div className="flex justify-between flex-row">
+          <div>
+            <h3>Buy it. Add it. Relax.</h3>
+            <h3>Get smart reminders before your food goes bad.</h3>
+          </div>
+          <div>
+            <div
+              className="flex min-h-svh flex-col items-center justify-center"
+              onClick={() => alert("The Button is clicked")}
+            >
+              <Button className="bg-black text-blue-200">Sign up</Button>
+            </div>
+            <div
+              className="flex min-h-svh flex-col items-center justify-center"
+              onClick={() => alert("The Button is clicked")}
+            >
+              <Button className="bg-black text-blue-200">Log in </Button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
