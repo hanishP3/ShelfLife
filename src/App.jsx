2370424c602bf4d9ab3a3c596a73ea68baacee31
hanish <phanish3000@gmail.com>
2025-07-17 23:32:59 +0530

    Background color

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,20 @@ function App() {
         <h1 className="text-5xl text-blue-50 ml-2.5">Shelf Life</h1>
       </div>
       <div className="h-lvh">
-        <div className="flex justify-between flex-row">
+        <div className="flex justify-between flex-col ">
           <div>
             <h3>Buy it. Add it. Relax.</h3>
             <h3>Get smart reminders before your food goes bad.</h3>
           </div>
-          <div>
+          <div className="bg-amber-400">
             <div
-              className="flex min-h-svh flex-col items-center justify-center"
+              className="flex  flex-col items-center justify-center"
               onClick={() => alert("The Button is clicked")}
             >
               <Button className="bg-black text-blue-200">Sign up</Button>
             </div>
             <div
-              className="flex min-h-svh flex-col items-center justify-center"
+              className="flex  flex-col items-center justify-center"
               onClick={() => alert("The Button is clicked")}
             >
               <Button className="bg-black text-blue-200">Log in </Button>
