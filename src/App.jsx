4b1ad08f0e5f0eda5e2e9e12e75dd9539e34637a
hanish <phanish3000@gmail.com>
2025-07-25 23:32:31 +0530

    add info for body

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ function App() {
       <div className="h-lvh">
         <main
           className="flex h-lvh items-center justify-center px-10
-  bg-[linear-gradient(0deg,#0E2954,#000000)]
-  bg-[length:300%_300%]
+  bg-[linear-gradient(270deg,#0E2954,#000000,#0E2954)]
+  bg-[length:400%_400%]
   animate-gradient-soft"
         >
           <Header></Header>
--- src/components/ui/Body.jsx
+++ src/components/ui/Body.jsx
@@ -9,25 +9,40 @@ import {
 function Body() {
   return (
     <>
-      <div className="h-[70vh]">
-        <div className="w-[80vh] justify-center align-middle items-center max-h-3/5">
-          <div className="w-[80vh] flex justify-center items-center">
-            <Carousel>
-              <CarouselContent>
-                <CarouselItem className="flex justify-center items-center">
-                  HI
-                </CarouselItem>
-                <CarouselItem className="flex justify-center items-center">
-                  HELLO
-                </CarouselItem>
-                <CarouselItem className="flex justify-center items-center">
-                  HOW ARE YOU
-                </CarouselItem>
-              </CarouselContent>
-              <CarouselPrevious />
-              <CarouselNext />
-            </Carousel>
-          </div>
+      <div className="flex justify-center items-center h-[70vh]">
+        <div className="w-[90vw]">
+          <Carousel className="bg-amber-600">
+            <CarouselContent>
+              <CarouselItem className="flex justify-center items-center h-[60vh] w-[20vw]">
+                <img
+                  src="public\foodWastage.jpg"
+                  alt="Placeholder 1"
+                  className="rounded-md size-50"
+                />
+                <h2>Heading: The Problem</h2>
+                <h4>
+                  Every day, huge portions of our food stock end up wasted
+                  simply because we forget what’s sitting on our shelves.
+                  Perfectly good meals are thrown away, while millions struggle
+                  without enough to eat.
+                </h4>
+              </CarouselItem>
+              <CarouselItem className="flex justify-center items-center h-[60vh] w-[20vw]">
+                <h2> Heading: The Effect</h2>
+                <h4>
+                  This constant wastage of food stock not only hurts our wallets
+                  but also harms our planet, adding to landfills and producing
+                  harmful gases that speed up climate change
+                </h4>
+              </CarouselItem>
+              <CarouselItem className="flex justify-center items-center h-[60vh] w-[20vw]">
+                <h2> Heading: Our Solution</h2>
+                <h4>Shelf Life is here to care for your food stock by tracking expiry dates and giving you gentle reminders, so you can use everything you buy and reduce unnecessary waste in your daily life.</h4>
+              </CarouselItem>
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
         </div>
       </div>
     </>
