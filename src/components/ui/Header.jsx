1445a416ec772525060e0211585a2156762e18da
hanish <phanish3000@gmail.com>
2025-08-07 00:04:23 +0530

    add a animation to the main image

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -8,36 +8,40 @@ export default function Header() {
   return (
     <div className="flex flex-row justify-between items-center w-[100%] align-center">
       <div>
-       <img src="public/mainbg2.png" alt="womenSearching" class="w-[500px] h-[500px] ml-25" />
+        <img
+          src="public/mainbg2.png"
+          alt="womenSearching"
+          className="w-[500px] h-[500px] ml-25 animate-slide-in-left"
+        />
       </div>
       <div>
         <h3 className="main-heading-fontSize-logo">Shelf Life</h3>
-      <h4 className="main-heading-fontSize">Buy it. Add it. Relax.</h4>
-      <h3 className="main-subHeading-font">
-        Get smart reminders before your stock goes bad,
-        <br />
-        because every item counts.
-      </h3>
-      <div className="flex flex-row gap-10 my-3  ">
-        <div
-          className="flex  "
-          onClick={() => {
-            console.log("Redirecting to login page");
-            navigation("/login");
-          }}
-        >
-          <Button className="main-logginButtion-styles">Sign up</Button>
+        <h4 className="main-heading-fontSize">Buy it. Add it. Relax.</h4>
+        <h3 className="main-subHeading-font">
+          Get smart reminders before your stock goes bad,
+          <br />
+          because every item counts.
+        </h3>
+        <div className="flex flex-row gap-10 my-3  ">
+          <div
+            className="flex  "
+            onClick={() => {
+              console.log("Redirecting to login page");
+              navigation("/login");
+            }}
+          >
+            <Button className="main-logginButtion-styles">Sign up</Button>
+          </div>
+          <div
+            className="flex"
+            onClick={() => {
+              console.log("Redirecting to login page");
+              navigation("/login");
+            }}
+          >
+            <Button className="main-logginButtion-styles">Log in </Button>
+          </div>
         </div>
-        <div
-          className="flex"
-          onClick={() => {
-            console.log("Redirecting to login page");
-            navigation("/login");
-          }}
-        >
-          <Button className="main-logginButtion-styles">Log in </Button>
-        </div>
-      </div>
       </div>
     </div>
   );
