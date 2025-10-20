3ada39e591bca1f4d19d6dc6f3c8be31889560f8
hanish <phanish3000@gmail.com>
2025-10-20 18:51:40 +0530

    new landing page with responsive ness

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -7,7 +7,7 @@ function LandingPage() {
     console.log("All letters have animated!");
   };
   return (
-    <div className="flex h-[100%] w-7xl justify-center ">
+    <div className="flex h-[100%] w-7xl justify-center">
       <div className="flex w-[80%] items-center justify-between">
         <AnimatedContent
           distance={200}
@@ -24,13 +24,13 @@ function LandingPage() {
           <img
             src="dist/images/mainbg2.png"
             alt="landing-image"
-            className="h-[80vh]"
+            className="hidden h-[150vh] min-w-[50%] flex-shrink-0 object-contain sm:block md:block"
           />
         </AnimatedContent>
-        <div>
+        <div className="leading-10">
           <SplitText
             text="Shelflife"
-            className="text-center text-8xl font-mainHeading"
+            className="font-mainHeading text-center text-5xl sm:text-7xl md:text-6xl lg:text-9xl"
             delay={100}
             duration={0.6}
             ease="power3.out"
@@ -42,7 +42,24 @@ function LandingPage() {
             textAlign="center"
             onLetterAnimationComplete={handleAnimationComplete}
           />
-          <p className="font-mainHeading">Lorem ipsum dolor sit amet.</p>
+          <h3 className="main-subHeading-font text-2xl md:text-2xl lg:text-4xl">
+            Get{" "}
+            <span className="bg-blue-400 px-2 font-light text-white">
+              Smart Reminders
+            </span>{" "}
+            before your stock goes bad, because every item counts.
+          </h3>
+          <div className="mt-3 flex flex-col items-center gap-3 sm:flex-row">
+            {/* Primary CTA */}
+            <button className="flex w-full items-center justify-center gap-2 rounded-md bg-[#85412E] px-6 py-2 text-2xl font-normal text-white transition-all duration-300 hover:scale-105 hover:bg-[#6E3526] sm:w-auto">
+              Start Adding Now
+            </button>
+
+            {/* Secondary CTA */}
+            <button className="flex w-full items-center justify-center rounded-md bg-blue-500 px-6 py-2 text-2xl font-normal text-white transition-all duration-300 hover:scale-105 hover:bg-blue-600 sm:w-auto">
+              Login
+            </button>
+          </div>
         </div>
       </div>
     </div>
